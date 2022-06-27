bb73c36c127daac1b03e3112c21f46860d57d754
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-06-27 00:28:44 -0500

    my forEach loop is working

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,26 @@
 // 1) display current day- I can do that in the <p> id currentDay
 console.log("this is working");
 var currentDayEl = document.querySelector("#currentDay");
+var hour = ['8:00am', 
+            '9:00am', 
+            '10:00am', 
+            '11:00am',
+            '12:00pm',
+            '1:00pm',
+            '2:00pm',
+            '3:00pm',
+            '4:00pm',
+            '5:00pm'
+        ]
 setInterval(function() {
     currentDayEl.textContent = moment().format("dddd, MMMM Do YYYY");
-},1000)
+},1000);
+
+// 2) display the 10 hour in the workday from 8-5
+
+var timeBlockP = $("<p>")
+hour.forEach(function() {
+    $(".container").append(timeBlockP);
+    console.log("my loop is working");
+});
+
